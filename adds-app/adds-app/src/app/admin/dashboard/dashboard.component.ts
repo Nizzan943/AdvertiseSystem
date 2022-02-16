399631288328a21c5f44072e2cdd4029c5481022
Nitzan Miranda <Nizzan943@gmail.com>
2022-02-16 23:56:41 +0200

    admin dashbaord client update automatically

--- adds-app/adds-app/src/app/admin/dashboard/dashboard.component.ts
+++ adds-app/adds-app/src/app/admin/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {AdminService} from "../admin.service";
-import {MatTableDataSource} from "@angular/material/table";
-import {Router} from "@angular/router";
-import {Client} from "../../shared/shared";
+import { AdminService } from '../admin.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Client } from '../../shared/shared';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   clients: Client[];
@@ -17,14 +17,21 @@ export class DashboardComponent implements OnInit {
   constructor(private adminService: AdminService, private router: Router) {}
 
   ngOnInit(): void {
-    this.adminService.getClients().subscribe(clients => {
+    this.adminService.getClients().subscribe((clients) => {
       console.log(clients);
       this.clients = clients;
       this.dataSource.data = this.clients;
-    })
+    });
+
+    setInterval(() => {
+      this.adminService.getClients().subscribe((clients) => {
+        this.clients = clients;
+        this.dataSource.data = this.clients;
+      });
+    }, 1000);
   }
 
-  toClient(client: Client){
-    this.router.navigate([`dashboard/client/${client.id}`])
+  toClient(client: Client) {
+    this.router.navigate([`dashboard/client/${client.id}`]);
   }
 }
